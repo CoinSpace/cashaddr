ab0712494c99ef73fe26a69639f9957ef2fcca23
Evgeny Vlasenko <mahnunchik@gmail.com>
2025-10-21 17:55:35 +0100

    feat: migrate to bech32cashaddr

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import { Address as BTCAddress } from '@scure/btc-signer';
-import { utils } from '@scure/base';
-
-const BECH_ALPHABET = utils.chain(
-  utils.alphabet('qpzry9x8gf2tvdw0s3jn54khce6mua7l'),
-  utils.join('')
-);
+import { cashaddr } from 'bech32cashaddr';
 
 /**
  * @typedef {Object} Network
@@ -49,104 +44,6 @@ export const TEST_NETWORK = {
   scriptHash: 0xc4,
   prefix: 'bchtest',
 };
-
-const POLYMOD_GENERATORS_CASHADDR = [
-  0x98f2bc8e61n,
-  0x79b76d99e2n,
-  0xf33e5fb3c4n,
-  0xae2eabe2a8n,
-  0x1e4f43e470n,
-];
-
-/**
- * @param {bigint} pre
- * @returns {bigint}
- */
-function cashaddrPolymod(pre) {
-  const b = pre >> 35n;
-  let chk = (pre & 0x07ffffffffn) << 5n;
-  for (let i = 0; i < POLYMOD_GENERATORS_CASHADDR.length; i++) {
-    if (((b >> BigInt(i)) & 1n) === 1n) chk ^= POLYMOD_GENERATORS_CASHADDR[i];
-  }
-  return chk;
-}
-
-/**
- * @param {string} prefix
- * @param {number[]} words
- * @returns {string}
- */
-function cashaddrChecksum(prefix, words) {
-  const len = prefix.length;
-  let chk = 1n;
-  for (let i = 0; i < len; i++) {
-    const c = prefix.charCodeAt(i);
-    if (c < 33 || c > 126) throw new Error(`Invalid prefix (${prefix})`);
-    chk = cashaddrPolymod(chk) ^ (BigInt(c) & 0x1fn);
-  }
-  chk = cashaddrPolymod(chk);
-  for (const v of words) chk = cashaddrPolymod(chk) ^ BigInt(v);
-  for (let i = 0; i < 8; i++) chk = cashaddrPolymod(chk);
-  chk ^= 1n;
-  const checksumWords = utils.convertRadix([Number(chk % 2n ** 40n)], 2 ** 40, 2 ** 5);
-  while (checksumWords.length < 8) checksumWords.unshift(0);
-  return BECH_ALPHABET.encode(checksumWords);
-}
-
-function genCashaddr() {
-  const _words = utils.radix2(5);
-
-  /**
-   * @param {string} prefix
-   * @param {number[] | Uint8Array} words
-   * @param {boolean} [includePrefix=true]
-   * @returns {string}
-   */
-  function encode(prefix, words, includePrefix = true) {
-    if (typeof prefix !== 'string') {
-      throw new TypeError(`cashaddr.encode: prefix should be string, not ${typeof prefix}`);
-    }
-    if (!Array.isArray(words) || (words.length && typeof words[0] !== 'number')) {
-      throw new TypeError(`cashaddr.encode: words should be array of numbers, not ${typeof words}`);
-    }
-    if (prefix.length === 0) {
-      throw new TypeError(`cashaddr.encode: invalid prefix length ${prefix.length}`);
-    }
-    const lowered = prefix.toLowerCase();
-    const sum = cashaddrChecksum(lowered, words);
-    const payload = `${BECH_ALPHABET.encode(words)}${sum}`;
-    return includePrefix ? `${lowered}:${payload}` : payload;
-  }
-
-  /**
-   * @param {string} str
-   * @returns {CashaddrDecoded}
-   */
-  function decode(str) {
-    if (typeof str !== 'string') {
-      throw new TypeError(`cashaddr.decode: input should be string, not ${typeof str}`);
-    }
-    const lowered = str.toLowerCase();
-    if (str !== lowered && str !== str.toUpperCase()) {
-      throw new TypeError('cashaddr.decode: string must be lowercase or uppercase');
-    }
-    const sepIndex = lowered.lastIndexOf(':');
-    if (sepIndex === 0 || sepIndex === -1) {
-      throw new TypeError('cashaddr.decode: symbol ":" must be present between prefix and data only');
-    }
-    const prefix = lowered.slice(0, sepIndex);
-    const payload = lowered.slice(sepIndex + 1);
-    if (payload.length < 8) throw new Error('Cashaddr data must be at least 8 characters long');
-    const words = BECH_ALPHABET.decode(payload).slice(0, -8);
-    const sum = cashaddrChecksum(prefix, words);
-    if (!payload.endsWith(sum)) throw new Error(`Invalid cashaddr checksum in ${str}: expected "${sum}"`);
-    return { prefix, words };
-  }
-
-  return { encode, decode, fromWords: _words.decode, toWords: _words.encode };
-}
-
-export const cashaddr = genCashaddr();
 
 export const CASHADDR_TYPE_BITS = ['pkh', 'sh'];
 export const CASHADDR_SIZE_BITS = [20, 24, 28, 32, 40, 48, 56, 64];
